b65e927ce76725b8db05dee30aa52baa6e81b1d0
namkyounghyun <david1510@naver.com>
2022-04-03 17:18:13 +0900

    Add test.pt

---
+++ 1546.py
@@ -0,0 +1,16 @@
+from cgitb import reset
+import sys
+a=int(input())
+score=list[a]
+
+score=list(map(int,sys.stdin.readline().split()))
+
+result_score=[]
+for i in score:
+    result_score.append((i/max(score))*100)
+
+sum=0
+for i in result_score:
+    sum=sum+i
+
+print(sum/len(result_score))
