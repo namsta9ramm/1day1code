129e7a73d449df3116638d7aea06ef5543ea66f1
namkyounghyun <david1510@naver.com>
2022-04-13 16:47:17 +0900

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-a=[2,3,4,5]
-while  in a:
-    print(2)
+def print_star(a):
+    ans_list=[]
+
+    for i in range(a):
+    
+        for j in range(a):
+            
+            ans_list.append('*')
+            
+            if a//3<=i<a//3+a//3 and a//3<=j<a//3+a//3:
+
+        print()
+    return print_star(a//3)
+
+k=int(input())
+print(print_star(k))
