4a752b46872263fca0441193c68129c85c9883e1
namkyounghyun <david1510@naver.com>
2022-04-16 18:10:33 +0900

    test.py

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-def print_star(a):
-    ans_list=[]
+new_list=[]
+for i in range(5):
+    for j in range(6):
+        a=int(input())
+        new_list.append(a)
 
-    for i in range(a):
-    
-        for j in range(a):
-            
-            ans_list.append('*')
-            
-            if a//3<=i<a//3+a//3 and a//3<=j<a//3+a//3:
-
-        print()
-    return print_star(a//3)
-
-k=int(input())
-print(print_star(k))
+print(new_list)
