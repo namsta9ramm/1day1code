06284900689a67dd2d831bea8f4cd8af80eda3a6
namkyounghyun <david1510@naver.com>
2022-04-19 11:12:40 +0900

    sort_test.py

--- 정렬.py/2751.py
+++ 정렬.py/2751.py
@@ -1,8 +1,25 @@
-a=int(input())
-ans_list=[]
-for i in range(a):
-    j=int(input())
-    ans_list.append(j)
-ans_list.sort()
-for k in ans_list:
-    print(k)
+array = [8,4,6,2,9,1,3,7,5]
+
+def merge_sort(array):
+	if len(array) < 2:
+		return array
+	mid = len(array) // 2
+	low_arr = merge_sort(array[:mid])
+	high_arr = merge_sort(array[mid:])
+
+	merged_arr = []
+	l = h = 0
+	while l < len(low_arr) and h < len(high_arr):
+		if low_arr[l] < high_arr[h]:
+			merged_arr.append(low_arr[l])
+			l += 1
+		else:
+			merged_arr.append(high_arr[h])
+			h += 1
+	merged_arr += low_arr[l:]
+	merged_arr += high_arr[h:]
+	print(merged_arr)
+	return merged_arr
+
+
+array = merge_sort(array)
