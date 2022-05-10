52b10bc05849fc4f5a70bb67cf616f4a13804b66
namkyounghyun <david1510@naver.com>
2022-05-10 14:48:42 +0900

    test.py

--- 정렬.py/11650.py
+++ 정렬.py/11650.py
@@ -3,11 +3,11 @@ a=int(input())
 list_1=[]
 for i in range(a):
     [x,y]=map(int,input().split())
-    list_1.append([x,y])
+    list_1.append([y,x])
 ans_list=sorted(list_1)
 
 for k in range(a):
-    print(ans_list[k][0],ans_list[k][1])
-    
+    print(ans_list[k][1],ans_list[k][0])
+
     
 
