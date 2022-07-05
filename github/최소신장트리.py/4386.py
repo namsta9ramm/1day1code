aa5a32b3a71f249ff5c201838ee7a765ff74bd97
namkyounghyun <david1510@naver.com>
2022-07-05 14:47:19 +0900

    1197.py

--- github/최소신장트리.py/4386.py
+++ github/최소신장트리.py/4386.py
@@ -1,7 +1,5 @@
 import math
-import sys
-#별자리 만들기
-#첫번쨰 줄에 별의 개수 n이 주어진다
+
 def find_parent(x):
     if parent[x] != x:
         parent[x] = find_parent(parent[x])
@@ -15,24 +13,29 @@ def union_parent(a, b):
         parent[b] = a
     else:
         parent[a] = b
-n=int(input())
-parent=[i for i in range(n+1)]
+
+n = int(input())
+parent = [i for i in range(n + 1)]
+
 stars = []
-total_cost = 0
-edges=[]
+edges = []
+result = 0
+
 for _ in range(n):
-    a, b= map(float, input().split())    
-    stars.append((a, b))
-for i in range(n-1): #v=3  m=0 , range(1,3) [[1,2],[2,3],[3,4]]
-    for j in range(i+1,n):
-        edges.append((math.sqrt((stars[i][0]-stars[j][0])**2+(stars[i][1]-stars[j][1])**2),i,j))
+    x, y = map(float, input().split())
+    stars.append((x, y))
+
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        edges.append((math.sqrt((stars[i][0] - stars[j][0])**2 + (stars[i][1] - stars[j][1])**2), i, j))
+
 edges.sort()
 
-for k in range(n):
-    cost, a, b = edges[k]
-   
-    if find_parent(a) != find_parent(b):
-        union_parent(a, b)
-        total_cost += cost
+for edge in edges:
+    cost, x, y = edge
 
-print(round(total_cost,2))
+    if find_parent(x) != find_parent(y):
+        union_parent(x, y)
+        result += cost
+
+print(round(result, 2))
