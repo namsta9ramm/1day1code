1273f2972100ea11bc4e63877c46c176e7464d44
namkyounghyun <david1510@naver.com>
2022-07-05 16:31:56 +0900

    18258.py

--- github/집합과맵.py/test.py
+++ github/집합과맵.py/test.py
@@ -1,9 +1,6 @@
-S=input()
-ans=[]
+from collections import deque
 
-for i in range(len(S)):  #i=0 (0,1/1,2/2,3/3,4/)
-    for j in range(i,len(S)):  #(0,5)
-        temp=S[i:j+1]
-        ans.append(temp)
-print(ans)
-print(len(ans))
+a=deque([1,2,3])
+a.append(2)
+print(a.popleft())
+print(a)
