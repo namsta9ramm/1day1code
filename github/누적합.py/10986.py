c916584f734ab806e354b5682f98f71dc691e5c7
namkyounghyun <david1510@naver.com>
2022-07-07 12:22:00 +0900

    10986.py

--- github/누적합.py/10986.py
+++ github/누적합.py/10986.py
@@ -1,14 +1,22 @@
-# 1,2 2,3 3,1 1,2 
-a,b=map(int,input().split())
-list1=list(map(int,input().split()))
-k=0
+import sys
+total, tar = map(int,sys.stdin.readline().split())
+
+tem = list(map(int,sys.stdin.readline().split()))
+#tem=[1,2,3,1,2]
+# list1=[5,2,2]
+list1= [0 for i in range(tar)]
+presum = 0
+
+list1[0] = 1
+
+
+for i in range(total):
+    presum=presum+tem[i]
+
+    list1[presum%tar] += 1
+    print(list1)
 ans=0
-for i in range(a): # i=0 (1,a)
-    for j in range(i+1,a+1):
-        k=sum(list1[i:j])
-        
-        if k%3==0:
-            ans=ans+1
-           
+for i in list1:
+    ans += i*(i-1)//2
 
 print(ans)
