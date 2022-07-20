f610f1c150d5b4f8ec2dad8fb483e7e8d786939a
namkyounghyun <david1510@naver.com>
2022-07-20 10:36:36 +0900

    9215.py

--- github/누적합.py/16139.py
+++ github/누적합.py/16139.py
@@ -11,7 +11,7 @@ arr[0][ord(name[0]) - 97] = 1
 for i in range(1, len(name)):
     arr[i][ord(name[i]) - 97] = 1
     for j in range(26):
-        arr[i][j] += arr[i - 1][j]
+        arr[i][j] = arr[i][j]+arr[i - 1][j]
 for i in range(n):
     a = input().split()
     if int(a[1]) > 0:
